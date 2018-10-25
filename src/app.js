638d491ed48e55ed4e74e49a271e8820aa5c21a7
konta220 <konta220@users.noreply.github.com>
2018-10-25 17:25:22 +0900

    人数の計算ロジックの追加

--- src/app.js
+++ src/app.js
@@ -2,64 +2,90 @@ function fillZero2(num) {
     return ('0' + parseInt(num)).slice(-2);
 }
 
+function fillZero3(num) {
+    return ('00' + parseInt(num)).slice(-3);
+}
+
+const intervalMilliSeconds = 10;
+
+Date.prototype.formatTime = function () {
+    const f = fillZero2;
+    return f(this.getHours()) + ":" + f(this.getMinutes()) + ":" + f(this.getSeconds());
+}
+
+Date.prototype.getAllHours = function () {
+    return this.getHours + (this.getMinutes() + this.getSeconds() / 60) / 60
+}
+
 var app = new Vue({
     el: '#app',
     data: {
-        /** 動作中=true */
         isRunning: false,
-        /** 開始時間 */
-        startDate: undefined,
-        /** 表示時間 */
         timerDate: new Date(0, 0, 0, 0, 0, 0, 0),
-        /** 0秒にリセット=true */
-        canResetTime: true,
-        /** 毎秒更新の関数オブジェクト */
         updateTimerInterval: undefined,
+        person: 2,
+        wage: 850,
+        personalWage: 0,
     },
     computed: {
         time: function () {
-            const date = this.timerDate;
-            const f = fillZero2;
-            return f(date.getHours()) + ":" + f(date.getMinutes())
-                + ":" + f(date.getSeconds());
+            return this.timerDate.formatTime();
+        },
+
+        milliseconds: function () {
+            return fillZero2(this.timerDate.getMilliseconds() / 10);
         },
 
         runnningBtn: function () {
             return (this.isRunning ? "Stop" : "Start");
         },
-
     },
     methods: {
-        clearBtnClick: function () {
-            this.canResetTime = true;
+
+        clearBtnClick: function (event) {
             this.timerDate = new Date(0, 0, 0, 0, 0, 0, 0);
         },
         runBtnClick: function (event) {
 
-            if (this.canResetTime) {
-                this.startDate = new Date();
-                this.canResetTime = false;
-            }
-
             if (this.isRunning) {
                 clearInterval(this.updateTimerInterval);
+
             } else {
                 setIntervalUpdateTimerDate();
             }
 
             this.isRunning = !this.isRunning;
         },
+
+        personTime: function () {
+            const time = this.timerDate;
+            const person = this.person;
+
+            const personDate = new Date();
+            personDate.setHours(time.getHours() * person);
+            personDate.setMinutes(time.getMinutes() * person);
+            personDate.setSeconds(time.getSeconds() * person);
+            return personDate.formatTime();
+        },
+
+        wageMoney: function () {
+            const hours = this.timerDate.getAllHours();
+            const wageNum = this.wage;
+            return Math.floor(hours * 1000) / 1000 * wageNum;
+        },
+
+        allWage: function () {
+            return 1 * this.person;
+        }
     }
 });
 
 function setIntervalUpdateTimerDate() {
-
     updateTimerDate();
-    // １秒後の表示を更新
-    app.updateTimerInterval = setInterval(updateTimerDate, 1000);
+    app.updateTimerInterval = setInterval(updateTimerDate, intervalMilliSeconds);
 }
 
 function updateTimerDate() {
-    // setMillSecond
-    app.timerDate = new Date(0, 0, 0, 0, 0, 0, new Date() - app.startDate);
+    app.timerDate = new Date(app.timerDate);
+    app.timerDate.setMilliseconds(app.timerDate.getMilliseconds() + intervalMilliSeconds);
 }
